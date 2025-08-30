83f55f822216351b41a8c8b2485e01b5a66d4123
Hoangnhan809 <96624651+Hoangnhan809@users.noreply.github.com>
2025-08-30 17:12:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -13,29 +13,28 @@
     document.addEventListener('keydown', function(event) {
         // Duplicate Tab
         let alt=event.altKey,
-            crtl=event.ctrlKey,
+            ctrl=event.ctrlKey,
             shift=event.shiftKey;
         const currentUrl = window.location.href;
-        if (altK && event.key === 't') {
+        if (alt && event.key === 't') {
             event.preventDefault();
             GM_openInTab(currentUrl, { active: true });
         }
         //Open Gemini
-        if (altK && event.key === 'g') {
+        if (alt && event.key === 'g') {
             event.preventDefault();
             GM_openInTab("https://gemini.google.com/app?hl=vi", { active: true });
         }
         // Auto Capslock in Geogebra
         if (currentUrl === 'https://www.geogebra.org/geometry') {
             let c = event.key, x=c.charCodeAt(0);
-            if (alt && ctrl ) {
+            if (alt && ctrl) {
                 return ;
-            } 
+            }
             if (x > 64 && x < 91) {
                 x+=32;
                 document.execCommand('insertText',false,String.fromCharCode(x));
-            }
-            if (x > 96 && x < 123) {
+            } else if (x > 96 && x < 123) {
                 x-=32;
                 document.execCommand('insertText',false,String.fromCharCode(x));
             }
