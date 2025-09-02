aa5a52d8a2b6804155131871fac0a9a41e5034ff
Hoangnhan809 <96624651+Hoangnhan809@users.noreply.github.com>
2025-09-02 22:00:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // @name         Shortcut Keys
 // @namespace    http://tampermonkey.net/
 // @version      1.0
-// @description  Press Alt + T to duplicate the current tab.
+// @description  Some unuseful shortcut keys
 // @author       You
 // @match        *://*/*
 // @grant        GM_openInTab
@@ -12,24 +12,24 @@
     'use strict';
     document.addEventListener('keydown', function(event) {
         // Duplicate Tab
-        let alt=event.altKey,
-            ctrl=event.ctrlKey,
-            shift=event.shiftKey,
-            c = event.key;
+        let a=event.altKey,
+            c=event.ctrlKey,
+            s=event.shiftKey,
+            k = event.key;
         const currentUrl = window.location.href;
-        if (alt && c === 't') {
+        if (a && k === 't') {
             event.preventDefault();
             GM_openInTab(currentUrl, { active: true });
         }
         //Open Gemini
-        if (alt && c === 'g') {
+        if (a && k === 'g') {
             event.preventDefault();
             GM_openInTab("https://gemini.google.com/app?hl=vi", { active: true });
         }
         // Auto Capslock in Geogebra
         if (currentUrl === 'https://www.geogebra.org/geometry') {
-            let x = c.charCodeAt(0);
-            if (alt || ctrl || c.length>1) {
+            let x = k.charCodeAt(0);
+            if (a || c || k.length>1) {
                 return;
             } else {
                 event.preventDefault();
@@ -40,9 +40,10 @@
                     x+=32;
                     document.execCommand('insertText',false,String.fromCharCode(x));
                 } else {
-                    document.execCommand('insertText',false,c);
+                    document.execCommand('insertText',false,k);
                 }
             }
         }
+        //
     });
 })();
